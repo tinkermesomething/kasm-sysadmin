74880fb4cb75cdc7fa76db1fce6692114fe763f7
Angel Duran <130918774+tinkermesomething@users.noreply.github.com>
2024-05-17 11:52:23 +0100

    Update next.config.js
    Signed-off-by: Angel Duran <130918774+tinkermesomething@users.noreply.github.com>

--- site/next.config.js
+++ site/next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-sysadmin/1.0',
+  basePath: '/kasm-sysadmin/1.0/',
   trailingSlash: true,
   images: {
     unoptimized: true,
